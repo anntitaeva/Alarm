f27dbf428bf1e2474cc0f6fc6f7036ce720fb900
Анна Титаева <avtitaeva@edu.hse.ru>
2016-03-08 17:46:59 +0300

    setalarm

--- Arduino-Alarm/SetAlarm/SetAlarmViewModel.cs
+++ Arduino-Alarm/SetAlarm/SetAlarmViewModel.cs
@@ -1,28 +1,51 @@
 ﻿using Arduino_Alarm.Entities;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Arduino_Alarm.SetAlarm
 {
-    class SetAlarmViewModel:Factory //наследует первоначальные настройки, если изменяются, то файл перезаписывается
+    class SetAlarmViewModel:Factory, INotifyPropertyChanged //наследует первоначальные настройки, если изменяются, то файл перезаписывается, только это доделать
     {
+        InitialData data = new InitialData();
+
         public string Address { get; set; }
         public List<string> Transport { get; set; }
         public string TimeToReady { get; set; }
         public List<string> Minor { get; set; }
         public List<int> Subgroup { get; set; }
 
-        InitialData data = new InitialData();
-        //если сеттинг не нал, то данные из сеттинга
+        public int SelectedTransport { get; set;}
+        public int SelectedMinor { get; set; }
+        public int SelectedGroup { get ; set; }
+
+        
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        
 
         public SetAlarmViewModel()
         {
             Transport = data._transportMode;
+            OnPropertyChanged("Transport");
             Minor = data._mainors;
+            OnPropertyChanged("Minor");
             Subgroup = data._groups;
+            OnPropertyChanged("Subgroup");
+            SelectedGroup = -1;
+            SelectedMinor = -1;
+            SelectedTransport = -1;
+  
+        }
+
+        private void OnPropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
         }
 
     }
